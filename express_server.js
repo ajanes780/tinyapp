ee2c9a25bfd4e40615f11f6ba662eb37751ed36c
Aaron Janes <Aaron.Janes@gmail.com>
2021-01-18 09:54:24 -0700

    added template engine

--- express_server.js
+++ express_server.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+app.set("view engine", "ejs"); // template engine
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -15,7 +19,15 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get("/set", (req, res) => {
+  const a = 1;
+  res.send(`a = ${a}`);
+ });
+ 
+ app.get("/fetch", (req, res) => {
+  res.send(`a = ${a}`);
 
+});
 
 
 
