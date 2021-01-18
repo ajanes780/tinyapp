b43e12da0c698554a52aff4a557d7f95b140065e
Aaron Janes <Aaron.Janes@gmail.com>
2021-01-18 14:51:41 -0700

    added random number genrator

--- express_server.js
+++ express_server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+
+const bodyParser = require("body-parser"); // for post requests 
+app.use(bodyParser.urlencoded({extended: true}));
+
+
 app.set("view engine", "ejs"); // template engine
 
 const urlDatabase = {
@@ -20,13 +25,26 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new"); // form addition 
+});
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
-
-
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+
+function generateRandomString() {
+   const rando = Math.random().toString(20).substr(2, 6)
+console.log(rando);
+   return rando
+}
+generateRandomString()
